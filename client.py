e2096fd0cdc4c02aa7df4f24c8bc89b80dde37b8
tagirium <50774982+tagirium@users.noreply.github.com>
2020-09-21 13:34:05 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+import tqdm
+import sys
+import os
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
+
+host = "3.90.169.240"
+port = 22
+
+filename = sys.argv[1]
+filesize = os.path.getsize(filename)
+s = socket.socket()
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.")
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+    for _ in progress:
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            break
+        s.sendall(bytes_read)
+        progress.update(len(bytes_read))
+
+s.close()
